47006401c0577d85d2bb997c31719b04c206375b
naseongcheon <ch0226xz@nate.com>
2022-08-13 20:24:44 +0900

    second

--- A20092398/src/main/java/net/skhu/ThirdController.java
+++ A20092398/src/main/java/net/skhu/ThirdController.java
@@ -2,10 +2,12 @@ package net.skhu;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/third")
+@RequestMapping("third")
 public class ThirdController {
 
 	@RequestMapping("test1")
@@ -19,4 +21,16 @@ public class ThirdController {
 		return "third/test1";
 	}
 
+
+	@GetMapping("test2")
+	public String test2(Model model) {
+		model.addAttribute("student", new Student());
+		return "third/test2";
+	}
+
+	@PostMapping("test2")
+	public String test2(Model model, Student student) {
+		model.addAttribute("message", "저장되었습니다");
+		return "third/test2";
+	}
 }
